07de114a93260639ca654e2ac87e8fd3bc66bd1f
Aras CHAIGNE <aras.chaigne@etu.univ-nantes.fr>
2022-08-16 21:58:27 +0200

    [EDIT] the notif now specifies the city

--- src/msg.py
+++ src/msg.py
@@ -7,9 +7,10 @@ async def make_msg(accomodations: list[dict]) -> None:
         rent = acc["occupationModes"][0]["rent"]["max"]
         residence = acc["residence"]["label"]
         id = acc["id"]
+        city = acc["residence"]["sector"]["label"]
         booking_link = f"https://trouverunlogement.lescrous.fr/tools/residual/26/accommodations/{id}"
 
-        await dscrd.notifier(f"**Nouveau Logement**:\n{residence}\n{rent/100}€/mois\n{area}m²\n{booking_link}")
+        await dscrd.notifier(f"**Nouveau Logement**:\n{city} - {residence}\n{rent/100}€/mois\n{area}m²\n{booking_link}")
 
 async def tell_no_token() -> None:
 

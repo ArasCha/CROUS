1522b001738d94d851ced03eb82f767123fed5e5
CHAIGNE Aras <aras.chaigne@cimpa.com>
2024-07-17 17:55:05 +0200

    [EDIT] listing and booking free wished accomodations

--- src/logements.py
+++ src/logements.py
@@ -12,19 +12,24 @@ api_version: 27 means year 2022-2023 and 31 year 2023-2024 (32 is 2023-2024 but 
 36 for 2024-2025
 """
 
-
 async def prg() -> None:
     
     async with CrousSession(36) as crous:
 
         try:
-            data = await crous.get_free_accomodations()
+            free_acc = await crous.get_free_accommodations()
             
             with open("../available.json", "w", encoding="utf-8") as f:
-                new_data = json.dumps(data)
+                new_data = json.dumps(free_acc)
                 f.write(new_data)
             
-            await make_msg(data)
+            [await crous._add_accommodation_to_selection(acc["id"]) for acc in free_acc]
+            [await tell_accommodation_listed(acc) for acc in free_acc]
+            
+            bookable_acc = list(filter(is_bookable, free_acc))
+            [await crous.book_accommodation(acc["id"]) for acc in bookable_acc]
+            [await tell_accommodation_booked(acc) for acc in bookable_acc]
+
 
         except TokenDead:
             await tell_no_token()
@@ -35,3 +40,11 @@ async def prg() -> None:
 
 async def is_token_ok(token:str) -> bool:
     return await crous_session.test_token(token)
+
+
+def is_bookable(accommodation: dict) -> list:
+    
+    post_codes = ["75005","75006"]
+    address = accommodation["residence"]["address"]
+    
+    return any(post_code in address for post_code in post_codes)

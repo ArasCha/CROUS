727e88a01ef4d97ebe195fc706603d38987a938f
CHAIGNE Aras <aras.chaigne@cimpa.com>
2024-07-17 14:38:30 +0200

    [FIX] accommodation address path

--- src/msg.py
+++ src/msg.py
@@ -7,7 +7,7 @@ async def make_msg(accomodations: list[dict]) -> None:
         rent = acc["occupationModes"][0]["rent"]["max"]
         residence = acc["residence"]["label"]
         id = acc["id"]
-        address = acc["address"]
+        address = acc["residence"]["address"]
         booking_link = f"https://trouverunlogement.lescrous.fr/tools/36/accommodations/{id}"
         
         await dscrd.notifier(f"**Nouveau Logement**:\n{residence} - {address}\n{rent/100}€/mois\n{area}m²\n{booking_link}")

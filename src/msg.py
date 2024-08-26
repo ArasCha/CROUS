4d930852d535ac1d9d9c220f32fda7e3ff09f556
CHAIGNE Aras <aras.chaigne@cimpa.com>
2024-08-26 14:50:35 +0200

    [FIX] message preparation

--- src/msg.py
+++ src/msg.py
@@ -1,29 +1,19 @@
 import dscrd
 
-def format_acc_msg(acc: dict) -> str:
-
-    area = acc["area"]["max"]
-    rent = acc["occupationModes"][0]["rent"]["max"]
-    residence = acc["residence"]["label"]
-    id = acc["id"]
-    address = acc["residence"]["address"]
-    
-    return f"{residence} - {address}\n{rent/100}€/mois\n{area}m²"
+def format_acc_msg(acc) -> str:
+    return f"{acc.residence_name} - {acc.address}\n{acc.max_rent/100}€/mois\n{acc.max_area}m²"
 
 
 async def tell_new_accommodation(accommodation) -> None:
-    formatted_accommodation = format_acc_msg(accommodation)
     booking_link = f"https://trouverunlogement.lescrous.fr/tools/36/accommodations/{accommodation['id']}"
-    await dscrd.notifier(f"**Nouveau logement**:\n{formatted_accommodation}\n{booking_link}")
+    await dscrd.notifier(f"**Nouveau logement**:\n{format_acc_msg(accommodation)}\n{booking_link}")
 
 async def tell_accommodation_listed(accommodation) -> None:
-    formatted_accommodation = format_acc_msg(accommodation)
     reservation_link = f"https://trouverunlogement.lescrous.fr/tools/36/cart"
-    await dscrd.notifier(f"**Logement ajouté à la liste**:\n{formatted_accommodation}\n{reservation_link}")
+    await dscrd.notifier(f"**Logement ajouté à la liste**:{format_acc_msg(accommodation)}\n{reservation_link}")
     
 async def tell_accommodation_booked(accommodation) -> None:
-    formatted_accommodation = format_acc_msg(accommodation)
-    await dscrd.notifier(f"**Logement demandé**:\n{formatted_accommodation}")
+    await dscrd.notifier(f"**Logement demandé**:\n{format_acc_msg(accommodation)}")
     
 
 async def tell_error(message: str) -> None:

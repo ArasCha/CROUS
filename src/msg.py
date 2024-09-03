9d787f8be74fea851f795cd6f541a8407a2db5a0
ArasChai <araschaigne@gmail.com>
2024-09-03 23:15:54 +0200

    [EDIT] messages display better

--- src/msg.py
+++ src/msg.py
@@ -6,22 +6,15 @@ def format_acc_msg(acc) -> str:
 booking_link_basis = "https://trouverunlogement.lescrous.fr/tools/36/accommodations/"
 
 async def tell_new_accommodation(accommodation) -> None:
-    booking_link = booking_link_basis + accommodation.id
-    await dscrd.notifier(f"""**Nouveau logement**:\n
-                         {format_acc_msg(accommodation)}\n
-                        {booking_link}""")
+    booking_link = booking_link_basis + str(accommodation.id)
+    await dscrd.notifier(f"**Nouveau logement**:\n{format_acc_msg(accommodation)}\n{booking_link}")
 
 async def tell_accommodation_listed(accommodation) -> None:
     reservation_link = f"https://trouverunlogement.lescrous.fr/tools/36/cart"
-    await dscrd.notifier(f"""**Logement ajouté à la liste**:\n
-                         {format_acc_msg(accommodation)}\n
-                        {reservation_link}\n
-                        {booking_link_basis}{accommodation.id}""")
+    await dscrd.notifier(f"**Logement ajouté à la liste**:\n{format_acc_msg(accommodation)}\n{reservation_link}\n{booking_link_basis}{accommodation.id}")
     
 async def tell_accommodation_booked(accommodation) -> None:
-    await dscrd.notifier(f"""**Logement demandé**:\n
-                         {format_acc_msg(accommodation)}\n
-                        {booking_link_basis}{accommodation.id}""")
+    await dscrd.notifier(f"**Logement demandé**:\n{format_acc_msg(accommodation)}\n{booking_link_basis}{accommodation.id}")
     
 
 async def tell_error(message: str) -> None:
